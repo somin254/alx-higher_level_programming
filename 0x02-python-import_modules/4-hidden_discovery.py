a7c31b4d06a8dbd22e41603ad6a4d59e86a61767
somin254 <sominrenson@gmai.com>
2024-01-04 07:27:10 -0800

    infinite

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     yote = dir()
     for i in range(0, len(yote)):
-            if yote[i][:2] != "_":
+        if yote[i][:2] != "__":
             print("{:s}".format(yote[i]))
-
