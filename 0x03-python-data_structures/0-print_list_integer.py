75a2566c3b7b6459608e15f0cdfba577ccf04af3
somin254 <sominrenson@gmai.com>
2024-01-07 04:44:37 -0800

    list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__name__":
+if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for num in my_list:
             print("{}".format(num))        
