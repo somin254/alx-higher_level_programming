659bd826f5164ff93f7aa6ed8a04e73f6e8b78f7
somin254 <sominrenson@gmai.com>
2024-01-07 05:53:27 -0800

    list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
-#!/usr/bin/prthon3
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+        for el in my_list[::-1]:
+            print("{:d}".format(el))
