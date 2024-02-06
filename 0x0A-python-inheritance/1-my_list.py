e8ce5c230026078696cf41068fedf1b684398dde
somin254 <sominrenson@gmail.com>
2024-02-05 18:25:16 -0800

    inheritance lookup

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#1/usr/bin/python3
+"""
+
+contain the mylist classs
+
+
+"""
+
+class mylist(list):
+    """a subclass of list"""
+    def __init__(self):
+        """ initializea the object """
+        super().__init__()
+
+        def print_sorted(self):
+        """prints the stored list"""
+        print(stored(self))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+ contain the class basegeometry
+ """
+ class BaseGeometry:
+     """An empty class """
+     pass
