29aca45dd5b3a1f304469ee54d80088e23686635
somin254 <sominrenson@gmail.com>
2024-04-08 13:41:10 -0700

    java is amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,2 @@
-!#/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+#!/usr/bin/node
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
