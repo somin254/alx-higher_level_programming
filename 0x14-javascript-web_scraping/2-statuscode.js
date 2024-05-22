df8d07428dc6c624afc3b28a4efc282050ff35d3
root <sominrenson@gamil.com>
2024-05-22 13:39:42 -0700

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-// Star Wars
-
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    console.log(content.title);
-  }
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
